3a70d0fbd3671b3a297afe0f2c5777b936470f0f
imxyy_soope_ <3516554684@qq.com>
2024-02-05 11:16:38 +0800

    feat: snap persistent (WIP)

--- nixos/impermanence/imxyy-nix.nix
+++ nixos/impermanence/imxyy-nix.nix
@@ -20,4 +20,26 @@
       ];
     };
   };
+
+  /* systemd.mounts = [
+    {
+      type = "btrfs";
+      options = "compress=zstd";
+      what = "/dev/nvme0n1p2";
+      where = "/btrfs_tmp";
+    }
+    ];
+    systemd.services.snap-persistent = {
+    description = "/persistent backup";
+    unitConfig.requiresMountsFor = "/btrfs_tmp";
+    script = ''mkdir -p /btrfs_tmp/persistent_backups; btrfs subvolume snapshot -r /btrfs_tmp/persistent "/btrfs_tmp/persistent_backups/$(date '+%Y-%m-%-d_%H:%M:%S')"; umount /btrfs_tmp; rmdir /btrfs_tmp'';
+    serviceConfig.Type = "oneshot";
+    startAt = "weekly";
+    };
+    systemd.timers.snap-persistent = {
+    timerConfig = {
+      RandomizedDelaySec = "0";
+      Persistent = true;
+    };
+  }; */
 }
