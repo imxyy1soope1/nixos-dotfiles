05cbac209612d12ea49f9f45b16bfcc77f2e390e
imxyy_soope_ <imxyy1soope1@gmail.com>
2025-04-18 17:40:57 +0800

    chore(wsl): disable vscode

--- config/hosts/imxyy-nix-wsl/home.nix
+++ config/hosts/imxyy-nix-wsl/home.nix
@@ -1,4 +1,4 @@
-{ username, ... }:
+{ lib, username, ... }:
 {
   my.home.programs.zsh.shellAliases = {
     localproxy_on = "export http_proxy=http://192.168.128.1:7890 https_proxy=http://192.168.128.1:7890 all_proxy=socks://192.168.128.1:7890";
@@ -6,6 +6,7 @@
   my = {
     sops.sshKeyPath = "/home/${username}/.ssh/id_ed25519";
     coding.all.enable = true;
+    coding.editor.vscode.enable = lib.mkForce false;
     cmd.misc.enable = true;
     xdg.enable = true;
     cmd.media.all.enable = true;
