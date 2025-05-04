1a7112c21d3f998f76f8c05c92e454c20b408daf
imxyy_soope_ <imxyy1soope1@gmail.com>
2025-05-04 10:57:03 +0800

    chore: remove useless comments

--- config/hosts/imxyy-nix/nixos.nix
+++ config/hosts/imxyy-nix/nixos.nix
@@ -111,56 +111,13 @@ in
   };
   environment.variables.NIX_REMOTE = "daemon";
 
-  # services.pipewire.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    # alsa.enable = true;
-    # alsa.support32Bit = true;
-    # pulse.enable = true;
     alsa.enable = false;
     alsa.support32Bit = false;
     pulse.enable = false;
     audio.enable = false;
-    configPackages = [
-      /*
-        (pkgs.writeTextDir "share/pipewire/media-session.d/bluez-monitor.conf" ''
-          rules = [
-            {
-              actions = {
-                update-props = {
-                  bluez5.autoswitch-profile = true;
-                }
-              }
-            }
-          ]
-        '')
-      */
-    ];
-  };
-  services.pipewire.wireplumber.extraConfig = {
-    /*
-      "10-bluez" = {
-        "monitor.bluez.properties" = {
-          "bluez5.enable-sbc-xq" = true;
-          "bluez5.enable-msbc" = true;
-          "bluez5.enable-hw-volume" = true;
-          "bluez5.roles" = [
-            "hsp_hs"
-            "hsp_ag"
-            "hfp_hf"
-            "hfp_ag"
-          ];
-        };
-      };
-    */
-    /*
-      "11-bluetooth-policy" = {
-        "wireplumber.settings" = {
-          "bluetooth.autoswitch-to-headset-profile" = false;
-        };
-      };
-    */
   };
   services.pulseaudio = {
     enable = true;
