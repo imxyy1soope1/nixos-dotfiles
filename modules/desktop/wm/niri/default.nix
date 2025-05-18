dbbafc5140923d32d4d8fe37a8fa403fd82ba7e1
imxyy_soope_ <imxyy1soope1@gmail.com>
2025-05-18 15:22:47 +0800

    fix: sddm autologin

--- modules/desktop/wm/niri/default.nix
+++ modules/desktop/wm/niri/default.nix
@@ -53,15 +53,7 @@ in
       sddm = {
         enable = true;
         wayland.enable = true;
-        theme = "where_is_my_sddm_theme";
-        extraPackages = [
-          (pkgs.where-is-my-sddm-theme.override {
-            variants = [ "qt6" ];
-            themeConfig.General = {
-              background = toString ./wallpaper.png;
-            };
-          })
-        ];
+        autoLogin.relogin = true;
       };
     };
     my.home = {
