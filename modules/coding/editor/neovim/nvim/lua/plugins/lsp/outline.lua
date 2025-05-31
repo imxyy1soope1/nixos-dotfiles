c477dbeae615103eb2a8fb3d0970516352ef047b
imxyy_soope_ <imxyy1soope1@gmail.com>
2025-05-31 15:32:57 +0800

    feat(nvim): better outline icon

--- modules/coding/editor/neovim/nvim/lua/plugins/lsp/outline.lua
+++ modules/coding/editor/neovim/nvim/lua/plugins/lsp/outline.lua
@@ -1,6 +1,9 @@
-M = {}
+M = {
+  symbols = {
+    icon_source = "lspkind"
+  }
+}
 
-vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
-{ desc = "Toggle Outline" })
+vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
 
 return M
