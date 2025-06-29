51b882f2c5fccf52ddc41919478409c2839d6b90
imxyy_soope_ <imxyy1soope1@gmail.com>
2025-06-29 15:14:08 +0800

    feat: add ~/.npm-global/bin to PATH

--- modules/coding/langs/js.nix
+++ modules/coding/langs/js.nix
@@ -24,6 +24,9 @@ lib.my.makeSwitch {
         prefix = ''${HOME}/.npm-global
         registry = https://registry.npmmirror.com
       '';
+      programs.zsh.initContent = lib.mkAfter ''
+        export PATH=$PATH:$HOME/.npm-global/bin
+      '';
     };
     my.persist.homeDirs = [
       ".npm"
