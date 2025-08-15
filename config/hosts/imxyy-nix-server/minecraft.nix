953952f3450bf8dc53db472553e94123c0da1942
imxyy_soope_ <imxyy1soope1@gmail.com>
2025-08-15 15:27:06 +0800

    feat: disable minecraft server :sob:

--- config/hosts/imxyy-nix-server/minecraft.nix
+++ config/hosts/imxyy-nix-server/minecraft.nix
@@ -1,16 +1,18 @@
 { lib, pkgs, ... }:
 {
-  systemd.services."fabric1.20.6" = {
-    description = "fabric 1.20.6 minecraft server";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" ];
-    serviceConfig = {
-      WorkingDirectory = "/opt/minecraft/fabric1.20.6";
-      ExecStart = "${lib.getExe' pkgs.openjdk21 "java"} -Xms1G -Xmx5G -jar fabric-server-mc.1.20.6-loader.0.15.11-launcher.1.0.1.jar";
-      Restart = "always";
-      RestartSec = 120;
+  /*
+    systemd.services."fabric1.20.6" = {
+      description = "fabric 1.20.6 minecraft server";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      serviceConfig = {
+        WorkingDirectory = "/opt/minecraft/fabric1.20.6";
+        ExecStart = "${lib.getExe' pkgs.openjdk21 "java"} -Xms1G -Xmx5G -jar fabric-server-mc.1.20.6-loader.0.15.11-launcher.1.0.1.jar";
+        Restart = "always";
+        RestartSec = 120;
+      };
     };
-  };
+  */
   my.persist = {
     nixosDirs = [
       "/opt/minecraft"
