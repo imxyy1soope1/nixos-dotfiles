dcfd134a4e8e722364ba8eb68cb10b5c7066ac42
imxyy_soope_ <imxyy1soope1@gmail.com>
2025-10-04 12:50:47 +0800

    feat(nvim): close nvim-tree when its the only window left

--- modules/coding/editor/neovim/nvim/lua/plugins/nvim-tree.lua
+++ modules/coding/editor/neovim/nvim/lua/plugins/nvim-tree.lua
@@ -17,4 +17,16 @@ local opt = require("core.globals").keymap_opt
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", opt)
 vim.keymap.set("n", "<leader>te", ":NvimTreeFocus<CR>", opt)
 
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("NvimTreeCloseOnLast", { clear = true }),
+  pattern = "NvimTree*",
+  callback = function()
+    if vim.api.nvim_call_function("winlayout", {})[1] == "leaf" and vim.bo.filetype == "NvimTree" then
+      vim.defer_fn(function()
+        vim.cmd("NvimTreeClose")
+      end, 10)
+    end
+  end,
+})
+
 return M
