8d7a4aed0151b1a8c0cd5c686ab4d59356a9a924
imxyy_soope_ <imxyy1soope1@gmail.com>
2025-12-07 11:51:19 +0800

    feat(direnv): configure

--- modules/coding/misc.nix
+++ modules/coding/misc.nix
@@ -17,7 +17,15 @@ lib.my.makeSwitch {
         gnumake
         github-cli # gh
       ];
-      programs.direnv.enable = true;
+      programs.direnv = {
+        enable = true;
+        config = {
+          global = {
+            warn_timeout = 0;
+            hide_env_diff = false;
+          };
+        };
+      };
     };
     my.persist.homeDirs = [
       ".local/share/direnv"
