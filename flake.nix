5988a98a09d9f63525c06237b9144a10d1cc49dd
imxyy_soope_ <imxyy1soope1@gmail.com>
2025-12-07 16:34:21 +0800

    feat: upgrade noctalia-shell

--- flake.nix
+++ flake.nix
@@ -61,7 +61,7 @@
     zen.inputs.nixpkgs.follows = "nixpkgs";
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell/v3.5.0";
+      url = "github:noctalia-dev/noctalia-shell/v3.6.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
