8d699f7b01b8accae2f5168cf66dbbdaf26a698c
imxyy_soope_ <imxyy1soope1@gmail.com>
2026-02-24 16:50:17 +0800

    chore: update flake.lock; update noctalia-shell

--- flake.nix
+++ flake.nix
@@ -81,7 +81,7 @@
     };
     niri.url = "github:sodiboo/niri-flake";
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell/v4.7.1";
+      url = "github:noctalia-dev/noctalia-shell/v4.7.5";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.noctalia-qs.inputs.treefmt-nix.follows = "";
     };
