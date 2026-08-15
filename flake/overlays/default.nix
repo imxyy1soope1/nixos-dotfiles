be47a92dd8231e3952986f9fe160c2a751ed03b0
imxyy_soope_ <imxyy1soope1@gmail.com>
2026-08-15 15:54:09 +0800

    flake.lock: update

--- flake/overlays/default.nix
+++ flake/overlays/default.nix
@@ -16,13 +16,6 @@
     lib.infuse prev {
       cage.__output.patches.__append = [ ./cage-specify-output-name.patch ];
       bottles.__input.removeWarningPopup.__assign = true;
-
-      jetbrains-mono.__assign = lib.warn ''
-        nanoemoji https://github.com/NixOS/nixpkgs/pull/552075
-      '' (final.mv.at "2026-08-12").jetbrains-mono;
-      moonlight-qt.__assign = lib.warn ''
-        moonlight-qt https://github.com/NixOS/nixpkgs/pull/552544
-      '' (final.mv.at "2026-08-10").moonlight-qt;
     }
   )
 ]
