8b141b9da09c4b712cf4aa031dcafaee06178e48
imxyy_soope_ <imxyy1soope1@gmail.com>
2026-08-29 09:35:46 +0800

    fmt: reformat *.toml with tombi, add tombi to treefmt.nix

--- treefmt.nix
+++ treefmt.nix
@@ -1,12 +1,27 @@
-{ inputs, ... }:
-{
-  imports = [ inputs.treefmt.flakeModule ];
-  perSystem.treefmt = {
+{ inputs, ... }: {
+  imports = [
+    inputs.treefmt.flakeModule
+  ];
+  perSystem.treefmt = { lib, pkgs, ... }: {
     projectRootFile = "flake.nix";
     programs = {
       nixfmt.enable = true;
-      stylua.enable = true;
       keep-sorted.enable = true;
+      stylua.enable = true;
     };
+    settings.formatter.tombi = {
+      command = "${pkgs.bash}/bin/bash";
+      options = [
+        "-euc"
+        ''
+          for file in "$@"; do
+            ${lib.getExe pkgs.tombi} fmt $file
+          done
+        ''
+        "--"
+      ];
+      includes = [ "*.toml" ];
+    };
+    settings.excludes = [ "secrets/*" ];
   };
 }
